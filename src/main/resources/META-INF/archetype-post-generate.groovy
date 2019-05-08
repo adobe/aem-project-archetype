4fcf16c0a9059220e21fbf34a5230d22de82c52e
hearora <hearora@hearora-WX-1.corp.adobe.com>
2019-05-08 16:51:09 +0530

    Add property to auto add config file for url mappings

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,8 +1,13 @@
 def optionIncludeErrorHandler = request.getProperties().get("optionIncludeErrorHandler")
+def optionIncludeUrlMapping = request.getProperties().get("optionIncludeUrlMapping")
 
 def rootDir = new File(request.getOutputDirectory() + "/" + request.getArtifactId())
 def uiAppsPackage = new File(rootDir, "ui.apps")
 
 if (optionIncludeErrorHandler == "n") {
     assert new File(uiAppsPackage, "src/main/content/jcr_root/apps/sling").deleteDir()
+}
+
+if (optionIncludeUrlMapping == "n") {
+    assert new File(uiAppsPackage, "src/main/content/jcr_root/apps/${appsFolderName}/config/org.apache.sling.jcr.resource.internal.JcrResourceResolverFactoryImpl.config").delete()
 }
