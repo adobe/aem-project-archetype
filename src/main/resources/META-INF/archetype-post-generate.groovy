225a44d2d77e6b5acc2d8b41696da4e7b29668a4
hearora <hearora@hearora-WX-1.corp.adobe.com>
2019-05-13 16:48:45 +0530

    Add landing and homepage template removed resource resolver config update etc map config removed option for url mapping in archetype

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,5 +1,4 @@
 def optionIncludeErrorHandler = request.getProperties().get("optionIncludeErrorHandler")
-def optionIncludeUrlMapping = request.getProperties().get("optionIncludeUrlMapping")
 
 def rootDir = new File(request.getOutputDirectory() + "/" + request.getArtifactId())
 def uiAppsPackage = new File(rootDir, "ui.apps")
@@ -8,7 +7,6 @@ if (optionIncludeErrorHandler == "n") {
     assert new File(uiAppsPackage, "src/main/content/jcr_root/apps/sling").deleteDir()
 }
 
-if (optionIncludeUrlMapping == "n") {
-    assert new File(uiAppsPackage, "src/main/content/jcr_root/apps/${appsFolderName}/config.publish").deleteDir()
+if (optionDomainName == "") {
     assert new File(uiAppsPackage, "src/main/content/jcr_root/etc/map.publish").deleteDir()
 }
