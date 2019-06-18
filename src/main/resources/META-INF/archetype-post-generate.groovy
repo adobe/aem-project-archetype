4eb8b1dbbb006741ce05a21372ea99cff8f9013a
Hemant Arora <hearora@adobe.com>
2019-06-18 16:24:22 +0530

    post script and required property update

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -4,10 +4,11 @@ import groovy.transform.Field
 
 @Field def rootDir = new File(request.getOutputDirectory() + "/" + request.getArtifactId())
 @Field def uiAppsPackage = new File(rootDir, "ui.apps")
-@Field def domainName = System.console().readLine 'Please enter domain name for url mapping [example.com]:'
+
 
 def optionIncludeErrorHandler = request.getProperties().get("optionIncludeErrorHandler")
 def optionRunmodeConfigs = request.getProperties().get("createRunmodeConfigs")
+def optionURlMapping = request.getProperties().get("createURlMapping")
 
 /**
  * Delete/Do not generate error handling files/folder
@@ -17,37 +18,24 @@ if (optionIncludeErrorHandler == "n") {
 }
 
 /**
+ * Delete/Do not generate url mapping files/folder
+ */
+if (optionURlMapping == "n") {
+   assert new File(uiAppsPackage, "src/main/content/jcr_root/apps/${appsFolderName}/config.publish").deleteDir()
+}
+
+/**
  * Generate runmode config files/folder
  */
 if (optionRunmodeConfigs == 'y') {
     createRunModeConfigs()
 }
 
-createUrlMapping()
-
 /**
- * Write a String to a file with the given name in the given directory
- */
-def writeToFile(File dir, String fileName, String content) {
-    FileUtils.write(new File(dir, fileName), content, "UTF-8")
-}
-
-/**
- * Create runmode configs during archetype execution based
- * on input from user. Also generate default externalizer configs  
+ * Create runmode configs during archetype execution for dev,qa,stage and prod envs  
  */
 def createRunModeConfigs() {
-
-    def externalizerFileContent = """\
-	# Configuration created by Apache Sling JCR Installer
-    externalizer.domains = ["local\\ http://localhost:4502", "author\\ https://author-${domainName}", "publish\\ https://${domainName}"]
-    externalizer.contextpath = ""
-    externalizer.host = ""
-    externalizer.encodedpath = B"false"
-    """
-
-    def envNames = System.console().readLine 'Please enter environment names for runmode configs (comma-delimited list)? [localdev,dev,qa,stage,prod]:'
-	if(envNames ==~ /(([a-z])+\,*)*/ && envNames.length()!=0){
+    def envNames = "dev,qa,stage,prod"
 		envNames = envNames.split(/,/) as String[]
 			println "Creating runmode config folders..."
 			    for (int i = 0; i < envNames.length; i++) {
@@ -55,41 +43,5 @@ def createRunModeConfigs() {
 			            authorDir.mkdir()
 			            def pubDir = new File(uiAppsPackage, "src/main/content/jcr_root/apps/${appsFolderName}/config.publish.${envNames[i]}")
 			            pubDir.mkdir()
-			            	if(domainName!=""){
-			            		writeToFile(authorDir, "com.day.cq.commons.impl.ExternalizerImpl.config", externalizerFileContent)
-			            		writeToFile(pubDir, "com.day.cq.commons.impl.ExternalizerImpl.config", externalizerFileContent)
-			            	}
 			    }
-	}else{
-		println 'please match expression (([a-z])+\\,*)*'
-		createRunModeConfigs()
-	}
-}
-
-/**
- * Create etc mapping during archetype execution based
- * on domain name input from user.  
- */
-def createUrlMapping() {
-    def etcMappingContent = """<?xml version="1.0" encoding="UTF-8"?>
-		<jcr:root xmlns:sling="http://sling.apache.org/jcr/sling/1.0" xmlns:jcr="http://www.jcp.org/jcr/1.0"
-	    jcr:primaryType="sling:Mapping"
-	    sling:internalRedirect="/content/${contentFolderName}/us/en"
-	    sling:match="^[^/]+/[^/]+/en"/>
-		"""
-    if (domainName != "") {
-        println "Creating etc mapping..."
-        def etcDir = new File(uiAppsPackage, "src/main/content/jcr_root/etc/map.publish/${domainName}")
-        etcDir.mkdir()
-        writeToFile(etcDir, ".content.xml", etcMappingContent)
-    } else {
-        assert new File(uiAppsPackage, "src/main/content/jcr_root/apps/${appsFolderName}/config.publish/org.apache.sling.jcr.resource.internal.JcrResourceResolverFactoryImpl.config").delete()
-        assert new File(uiAppsPackage, "src/main/content/jcr_root/etc/map.publish").deleteDir()
-        File filterXML = new File(uiAppsPackage, "/src/main/content/META-INF/vault/filter.xml")
-        def content = "<filter root=\"/etc/map.publish\" />"
-        def fileText = filterXML.text
-        fileText = fileText.replaceAll(content, "")
-        filterXML.write(fileText);
-        println "Update filter.xml..."
-    }
 }
