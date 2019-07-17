dda026e20d65ece397f9c73a8317835b15cad95f
Richard Hand <pid90611@adobe.com>
2019-07-17 19:22:13 +0200

    Integrate a dedicated frontend build
    - moves optionSkipFrontend -> optionIncludeFrontendProject.
    - switches default to frontend project disabled.
    - fixes issue in groovy post generate script for removal of the ui.frontend folder.
    - minor README improvements.

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,12 +1,12 @@
 def optionIncludeErrorHandler = request.getProperties().get("optionIncludeErrorHandler")
+def optionIncludeFrontendProject = request.getProperties().get("optionIncludeFrontendProject")
 
 def rootDir = new File(request.getOutputDirectory() + "/" + request.getArtifactId())
 def uiAppsPackage = new File(rootDir, "ui.apps")
-def uiFrontend = new File(rootDir, "ui.frontend")
 
 if (optionIncludeErrorHandler == "n") {
     assert new File(uiAppsPackage, "src/main/content/jcr_root/apps/sling").deleteDir()
 }
-if (optionSkipFrontend == "y") {
-    assert new File(uiFrontend).deleteDir()
+if (optionIncludeFrontendProject == "n") {
+    assert new File(rootDir, "ui.frontend").deleteDir()
 }
