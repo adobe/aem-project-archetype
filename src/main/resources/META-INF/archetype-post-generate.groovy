3e987cc9ac8da02fa3bec8dc14ae98905ebc96ab
gitrajat <rajat30.gupta@gmail.com>
2019-10-02 21:49:48 +0530

    Content Skeleton (#202)

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -4,20 +4,14 @@ def optionIncludeErrorHandler = request.getProperties().get("optionIncludeErrorH
 def optionIncludeFrontendModule = request.getProperties().get("optionIncludeFrontendModule")
 def optionAemVersion = request.getProperties().get("optionAemVersion")
 def confFolderName = request.getProperties().get("confFolderName")
+def contentFolderName = request.getProperties().get("contentFolderName")
+def language_country = request.getProperties().get("language_country")
+def isSingleCountryWebsite = request.getProperties().get("isSingleCountryWebsite")
 
 def rootDir = new File(request.getOutputDirectory() + "/" + request.getArtifactId())
 def rootPom = new File(rootDir, "pom.xml")
 def uiAppsPackage = new File(rootDir, "ui.apps")
 def uiContentPackage = new File(rootDir, "ui.content")
-
-def removeModule(pomFile, module) {
-    def pattern = Pattern.compile("\\s*<module>" + Pattern.quote(module) + "</module>", Pattern.MULTILINE)
-    def pomContent = pomFile.getText("UTF-8")
-    pomContent = pomContent.replaceAll(pattern, "")
-    pomFile.newWriter("UTF-8").withWriter { w ->
-        w << pomContent
-    }
-}
 
 if (optionIncludeErrorHandler == "n") {
     assert new File(uiAppsPackage, "src/main/content/jcr_root/apps/sling").deleteDir()
@@ -30,4 +24,48 @@ if (optionAemVersion == "6.3.3") {
 if (optionIncludeFrontendModule == "n") {
     assert new File(rootDir, "ui.frontend").deleteDir()
     removeModule(rootPom, "ui.frontend")
+}
+
+
+buildContentSkeleton(isSingleCountryWebsite, language_country, contentFolderName, uiContentPackage)
+
+/**
+ * Create content skeleton based upon isSingleCountry
+ * & languageCountry input from user
+ */
+def buildContentSkeleton(isSingleCountryWebsite, language_country, contentFolderName, uiContentPackage) {
+
+    println "Creating content skeleton..."
+    def contentDetails = language_country.split('_')
+
+    if (isSingleCountryWebsite == "y") {
+        def languageMastersDir = new File(uiContentPackage, "src/main/content/jcr_root/content/${contentFolderName}/language-masters")
+        languageMastersDir.deleteDir()
+
+    } else {
+        def languageDir = new File(uiContentPackage, "src/main/content/jcr_root/content/${contentFolderName}/language-masters/en")
+        languageDir.renameTo(new File(uiContentPackage, "src/main/content/jcr_root/content/${contentFolderName}/language-masters/${contentDetails[0]}"))
+
+    }
+
+    def countryDir = new File(uiContentPackage, "src/main/content/jcr_root/content/${contentFolderName}/us")
+    countryDir.renameTo(new File(uiContentPackage, "src/main/content/jcr_root/content/${contentFolderName}/${contentDetails[1]}"))
+
+    def languageInCountryDir = new File(uiContentPackage, "src/main/content/jcr_root/content/${contentFolderName}/${contentDetails[1]}/en")
+    languageInCountryDir.renameTo(new File(uiContentPackage, "src/main/content/jcr_root/content/${contentFolderName}/${contentDetails[1]}/${contentDetails[0]}"))
+}
+
+/**
+ * Removes a module section from the given pom file
+ * @param pomFile pom file where the module section should be removed from
+ * @param module name of the module which should be removed
+ * @return
+ */
+def removeModule(pomFile, module) {
+    def pattern = Pattern.compile("\\s*<module>" + Pattern.quote(module) + "</module>", Pattern.MULTILINE)
+    def pomContent = pomFile.getText("UTF-8")
+    pomContent = pomContent.replaceAll(pattern, "")
+    pomFile.newWriter("UTF-8").withWriter { w ->
+        w << pomContent
+    }
 }
