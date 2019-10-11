63cdacb507bde081610fa63cad5e83ebc1c99dd8
Vlad <vlad@bailescu.ro>
2019-10-11 08:40:43 +0300

    Dispatcher Module Support (#251)
    * Adding Dispatcher support logic. (#237)
    
    Co-authored-by: Bryan Stopp <bryan.stopp@gmail.com>

--- README.md
+++ README.md
@@ -93,6 +93,7 @@ optionIncludeExamples       |    y    | Include a Component Library example site
 optionIncludeErrorHandler   |    n    | Include a custom 404 response page
 optionIncludeFrontendModule |    n    | Include a dedicated frontend module
 isSingleCountryWebsite      |    y    | Create language-master structure in example content
+optionDispatcherConfig      |   none  | Generate a dispatcher configuration module
 
 Note: If the archetype is executed in interactive mode the first time properties with default values can't be changed (see 
 [ARCHETYPE-308](https://issues.apache.org/jira/browse/ARCHETYPE-308) for more details). The value can be changed when the property 
--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,18 +1,20 @@
+import static groovy.io.FileType.*
+import java.nio.file.Path
 import java.util.regex.Pattern
-import groovy.transform.Field
 
-@Field def rootDir = new File(request.getOutputDirectory() + "/" + request.getArtifactId())
-@Field def uiAppsPackage = new File(rootDir, "ui.apps")
-@Field def uiContentPackage = new File(rootDir, "ui.content")
+def rootDir = new File(request.getOutputDirectory() + "/" + request.getArtifactId())
+def uiAppsPackage = new File(rootDir, "ui.apps")
+def uiContentPackage = new File(rootDir, "ui.content")
 def rootPom = new File(rootDir, "pom.xml")
 
-@Field def isSingleCountryWebsite = request.getProperties().get("isSingleCountryWebsite")
-@Field def contentFolderName = request.getProperties().get("contentFolderName")
-@Field def language_country = request.getProperties().get("language_country")
+def isSingleCountryWebsite = request.getProperties().get("isSingleCountryWebsite")
+def contentFolderName = request.getProperties().get("contentFolderName")
+def language_country = request.getProperties().get("language_country")
 def optionIncludeErrorHandler = request.getProperties().get("optionIncludeErrorHandler")
 def optionIncludeFrontendModule = request.getProperties().get("optionIncludeFrontendModule")
 def optionAemVersion = request.getProperties().get("optionAemVersion")
 def confFolderName = request.getProperties().get("confFolderName")
+def optionDispatcherConfig = request.getProperties().get("optionDispatcherConfig")
 
 if (optionIncludeErrorHandler == "n") {
     assert new File(uiAppsPackage, "src/main/content/jcr_root/apps/sling").deleteDir()
@@ -27,12 +29,42 @@ if (optionIncludeFrontendModule == "n") {
     removeModule(rootPom, "ui.frontend")
 }
 
-buildContentSkeleton()
+buildContentSkeleton(uiContentPackage, uiAppsPackage, isSingleCountryWebsite, contentFolderName, language_country)
+
+if ( optionDispatcherConfig == "none" || optionDispatcherConfig == "n"  ) {
+    assert new File(uiAppsPackage, "src/main/content/jcr_root/apps/" + appsFolderName + "/config.publish").deleteDir()
+} else {
+    def source;
+    if ( optionDispatcherConfig == 'ams') {
+        source = new File(rootDir.getPath(), 'dispatcher.ams')
+    } else if (optionDispatcherConfig == 'cloud')   {
+        source = new File(rootDir.getPath(),'dispatcher.cloud')
+    }
+    assert source.renameTo(new File(rootDir.getPath(),'dispatcher'))
+    new File(rootDir, 'dispatcher').traverse(type: DIRECTORIES, nameFilter: ~/enabled.+$/) { it ->
+        it.traverse(type: FILES) { enabledFile ->
+            File availableFile = new File(enabledFile.getPath().replace('enabled', 'available'))
+            if (!availableFile.exists()) {
+                return;
+            }
+            Path target = java.nio.file.Paths.get(availableFile.getPath())
+            Path link = java.nio.file.Paths.get(enabledFile.getPath())
+            enabledFile.delete()
+            Path relativeSrc = link.getParent().relativize(target)
+            java.nio.file.Files.createSymbolicLink(link, relativeSrc)
+        }
+    }
+}
+assert new File(rootDir, 'dispatcher.ams').deleteDir()
+removeModule(rootPom, 'dispatcher.ams')
+assert new File(rootDir, 'dispatcher.cloud').deleteDir()
+removeModule(rootPom, 'dispatcher.cloud')
+
 
 /**
  * Creates content skeleton based upon isSingleCountry & languageCountry input from user
  */
-def buildContentSkeleton() {
+def buildContentSkeleton(uiContentPackage, uiAppsPackage, isSingleCountryWebsite, contentFolderName, language_country) {
     println "Creating content skeleton..."
     def contentDetails = language_country.split('_')
 
