b43691aa54ed57704979b6e19482866680f595ca
Daniel Gordon <dgordon@adobe.com>
2019-10-31 06:38:36 -0700

    Static Webpack Dev Server (#266)
    * includes a static webpack dev server and a static HTML template
    
    * moved Frontend readme information to ui.frontend module since not every project will have it

--- src/main/archetype/ui.frontend/webpack.dev.js
+++ src/main/archetype/ui.frontend/webpack.dev.js
@@ -1,8 +1,24 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const SOURCE_ROOT = __dirname + '/src/main/webpack';
 
 module.exports = merge(common, {
    mode: 'development',
    devtool: 'inline-source-map',
-   performance: {hints: "warning"}
+   performance: {hints: "warning"},
+   plugins: [
+      new HtmlWebpackPlugin({
+         template: path.resolve(__dirname, SOURCE_ROOT + '/static/index.html')
+      })
+   ],
+   devServer: {
+      inline: true,
+      proxy: [{
+         context: ['/content', '/etc.clientlibs'],
+         target: 'http://localhost:4502',
+      }]
+   }
 });
