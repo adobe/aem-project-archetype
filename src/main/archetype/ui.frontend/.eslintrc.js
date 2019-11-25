afb682e93c779906201d0b598101ceb7be3be727
Jim Baxter Matheson <jim.matheson@icidigital.com>
2019-11-25 09:50:01 -0500

    Deprecated TSLInt since it is officially deprecated in favor of ESLint with Typescript rules

---
+++ src/main/archetype/ui.frontend/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports =  {
+    parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+    extends:  [
+      'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    ],
+    parserOptions:  {
+      ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
+      sourceType:  'module',  // Allows for the use of imports
+    },
+    rules:  {
+        "curly": 1,
+        "@typescript-eslint/explicit-function-return-type": [0],
+        "@typescript-eslint/no-explicit-any": [0],
+        "ordered-imports": [0],
+        "object-literal-sort-keys": [0],
+        "max-len": [1, 120],
+        "new-parens": 1,
+        "no-bitwise": 1,
+        "no-cond-assign": 1,
+        "no-trailing-spaces": 0,
+        "eol-last": 1,
+        "func-style": ["error", "declaration", { "allowArrowFunctions": true }],
+        "semi": 1
+    },
+  };
