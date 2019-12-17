7f8533d89e6909e9dc671a59fc5366395d49e09e
Samuel Meuli <meuli@adobe.com>
2019-12-17 10:34:42 +0100

    Regenerate React app

---
+++ src/main/archetype/ui.frontend.react/src/components/Page/Page.js
@@ -0,0 +1,23 @@
+import {
+  MapTo,
+  Page,
+  withComponentMappingContext
+} from '@adobe/cq-react-editable-components';
+import { withRoute } from '../RouteHelper/RouteHelper';
+
+require('./Page.css');
+
+// This component is a variant of a Page component mapped to the "${appsFolderName}/components/structure/page" resource type
+// For now, the rendering is the same as the RootPage; this is more for illustration purposes
+class AppPage extends Page {
+  get containerProps() {
+    let attrs = super.containerProps;
+    attrs.className =
+      (attrs.className || '') + ' page ' + (this.props.cssClassNames || '');
+    return attrs;
+  }
+}
+
+export default MapTo('${appsFolderName}/components/structure/page')(
+  withComponentMappingContext(withRoute(AppPage))
+);
--- src/main/archetype/ui.frontend.react/src/index.js
+++ src/main/archetype/ui.frontend.react/src/index.js
@@ -1,32 +1,18 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- ~ Copyright 2018 Adobe Systems Incorporated
- ~
- ~ Licensed under the Apache License, Version 2.0 (the "License");
- ~ you may not use this file except in compliance with the License.
- ~ You may obtain a copy of the License at
- ~
- ~     http://www.apache.org/licenses/LICENSE-2.0
- ~
- ~ Unless required by applicable law or agreed to in writing, software
- ~ distributed under the License is distributed on an "AS IS" BASIS,
- ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- ~ See the License for the specific language governing permissions and
- ~ limitations under the License.
- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
 // Use polyfills for modern language features
 // The imports and dependencies can be removed if only modern browsers should be
 // supported
-import { Constants, ModelManager } from '@adobe/cq-spa-page-model-manager';
+import 'react-app-polyfill/stable';
+import 'react-app-polyfill/ie9';
 import 'custom-event-polyfill';
+
+import { Constants, ModelManager } from '@adobe/cq-spa-page-model-manager';
 import { createBrowserHistory } from 'history';
 import React from 'react';
-import 'react-app-polyfill/ie9';
-import 'react-app-polyfill/stable';
 import { render } from 'react-dom';
 import { Router } from 'react-router-dom';
-import App from './components/App';
+import App from './App';
 import './components/import-components';
+import './index.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   ModelManager.initialize().then(pageModel => {
