9acb05eb7e400db822b8df4be2261a8e7e5b8845
Samuel Meuli <meuli@adobe.com>
2019-12-19 15:08:20 +0100

    Fix Angular entrypoints

--- src/main/archetype/ui.frontend.angular/utils/entrypoints.js
+++ src/main/archetype/ui.frontend.angular/utils/entrypoints.js
@@ -1,11 +1,11 @@
 // Globs for entrypoint files (in the order they need to be required)
 const target = 'es5';
 const jsEntrypoints = [
-  `**/runtime-${target}.js`,
-  `**/polyfills-${target}.js`,
-  `**/styles-${target}.js`,
-  `**/vendor-${target}.js`,
-  `**/main-${target}.js`
+  `**/runtime-${target}*.js`,
+  `**/polyfills-${target}*.js`,
+  `**/styles-${target}*.js`,
+  `**/vendor-${target}*.js`,
+  `**/main-${target}*.js`
 ];
 const cssEntrypoints = ['**/*.css'];
 const entrypoints = [...jsEntrypoints, ...cssEntrypoints];
