e1e066fb4217d11964a8aef710eaeefd4ce14f31
austen-wade <austen.wade.ut@gmail.com>
2020-04-18 16:34:18 -0600

    Added 'watch' and 'aemsync' npm modules to sync frontend.general updates on default port

--- src/main/archetype/ui.frontend.general/webpack.dev.js
+++ src/main/archetype/ui.frontend.general/webpack.dev.js
@@ -5,20 +5,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const SOURCE_ROOT = __dirname + '/src/main/webpack';
 
-module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    performance: { hints: 'warning' },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, SOURCE_ROOT + '/static/index.html')
-        })
-    ],
-    devServer: {
-        inline: true,
-        proxy: [{
-            context: ['/content', '/etc.clientlibs'],
-            target: 'http://localhost:4502',
-        }]
-    }
-});
+module.exports = env => {
+    const writeToDisk = env && Boolean(env.writeToDisk);
+
+    return merge(common, {
+        mode: 'development',
+        devtool: 'inline-source-map',
+        performance: { hints: 'warning' },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, SOURCE_ROOT + '/static/index.html')
+            })
+        ],
+        devServer: {
+            inline: true,
+            proxy: [{
+                context: ['/content', '/etc.clientlibs'],
+                target: 'http://localhost:4502',
+            }],
+            writeToDisk,
+            liveReload: !writeToDisk
+        }
+    });
+}
