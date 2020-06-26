7cf5a014a2e92d8edc6e31397c27b6107259b528
Krystian Panek <krystian.panek@cognifide.com>
2020-06-26 08:14:37 +0200

    Regenerator script / tmp

---
+++ generate.sh
@@ -0,0 +1,12 @@
+rm -fr mysite
+
+mvn -B archetype:generate \
+ -D archetypeGroupId=com.adobe.granite.archetypes \
+ -D archetypeArtifactId=aem-project-archetype \
+ -D archetypeVersion=24-SNAPSHOT \
+ -D aemVersion=cloud \
+ -D appTitle="My Site" \
+ -D appId="mysite" \
+ -D groupId="com.mysite" \
+ -D frontendModule=general \
+ -D includeExamples=n
