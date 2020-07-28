70fafeb086996913d8bff7da3bf7fba8e94cdabd
David Catalan <40354404+catalan-adobe@users.noreply.github.com>
2020-07-28 09:39:55 +0200

    CQ-4299472 Add UI Testing submodule (#428)
    * CQ-4299472 Add UI Test module
    
    * Removed usages of term eaas
    
    * Remove debug logging
    
    * Update WDIO dependencies to latest major versions
    
    * Make file upload functions a WDIO custom command and add related Convention variables
    
    * Docker image reflects project coordinates
    
    * Improve upload file test case
    
    * Fix Copyright headers
    
    * Remove comment
    
    * Fox references to internal links in documentation
    
    * Refactor init of WDIO custom commands
    
    * Add test for ui.content sample page
    
    * Make tests agnostic to AEM language
    
    * Add linting feature
    
    * Linting
    
    * Fix specs list for local execution
    
    * Removed test not relevant for cloud context
    
    * Enforce shell script as execution permission
    
    * Only generate ui.tests module when AEM version is cloud
    
    * Add support in tests for browser headless mode
    
    * Add "Testing" feature
    
    * Add profile for local execution of the tests (without Docker)
    
    * Removed obsolete reference of ui.tests + list ordering
    
    * Use dynamic template values for Docker test image
    
    * Make ui.tests module available whatever aemVersion is used
    
    * Add test for sample content page
    
    * Add default value for AEM_AUTHOR_URL parameter
    
    * Documentation
    
    * Fix profile names
    
    * Make test reset changes after execution
    
    * Documentation
    
    * Fix test for Windows platform
    
    * Make test agnostic to AEM language
    
    * Fix indentation
    
    * Fix Docker image naming to support uppercases
    
    * Typo
    
    * Optimize WDIO configuration files
    
    * Add filename prefix parameter to the saveScreenshot function
    
    * Handle AEM Survey dialog
    
    * Fix Windows specifics
    
    * Increase default timeouts to handle slow first-time rendering of AEM pages
    
    * Disable "save password" popup in Chrome
    
    * Handle AEM tutorial popups
    
    * Minor refactor for headless mode
    
    * Comment
    
    * Minor refactor for headless mode
    
    * Extract Assets test in dedicated spec file
    
    * Increase WDIO default wait timeout
    
    * Refactor handling of user onboarding dialogs
    
    Co-authored-by: David Catalan <catalan@Davids-MacBook-Pro.local>
    Co-authored-by: David Catalan <catalan@davids-macbook-pro-1.home>
    Co-authored-by: David Catalan <catalan@davids-mbp-1.home>

---
+++ src/main/archetype/ui.tests/test-module/specs/aem/basic.js
@@ -0,0 +1,35 @@
+/*
+ *  Copyright 2020 Adobe Systems Incorporated
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+const config = require('../../lib/config');
+
+describe('AEM Basic', () => {
+
+    // AEM Login
+    beforeEach(() => {
+        browser.AEMForceLogout();
+        browser.url(config.aem.author.base_url);
+        browser.AEMLogin(config.aem.author.username, config.aem.author.password);
+    });
+
+    it('should be possible to display Solutions panel', () => {
+        browser.url(config.aem.author.base_url);
+
+        $('[data-foundation-toggleable-control-src$="solutionswitcher.html"]').click();
+
+        $('coral-shell-solutionswitcher').waitForDisplayed();
+    });
+
+});
