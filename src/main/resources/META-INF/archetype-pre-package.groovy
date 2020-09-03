747d27ba03c910d23d93656fe039c0cccdc46b17
Konrad Windszus <konrad.windszus@netcentric.biz>
2020-09-03 20:17:09 +0200

    make it work on Windows and non-Windows by calculating MD5 for both platforms

--- src/main/resources/META-INF/archetype-pre-package.groovy
+++ src/main/resources/META-INF/archetype-pre-package.groovy
@@ -1,9 +1,7 @@
 // custom groovy script executed prior to https://maven.apache.org/archetype/maven-archetype-plugin/jar-mojo.html
-import static groovy.io.FileType.*
-import groovy.util.XmlSlurper
-import java.nio.file.Path
 import java.security.MessageDigest
 import java.util.regex.Pattern
+import java.nio.charset.StandardCharsets
 
 def rootDir = properties['rootDir']
 insertImmutableDispatcherFileEnforcerRules(rootDir, 'dispatcher.ams', 'https://helpx.adobe.com/experience-manager/kb/ams-dispatcher-manual/immutable-files.html')
@@ -12,21 +10,26 @@ insertImmutableDispatcherFileEnforcerRules(rootDir, 'dispatcher.cloud', 'https:/
 def insertImmutableDispatcherFileEnforcerRules(rootDir, module, messageLink) {
     def target = new File(rootDir, module)
     def dispatcherPom = new File(target, 'pom.xml')
-    dispatcherPom.text = dispatcherPom.text.replaceAll('IMMUTABLE_RULES', getImmutableDispatcherFileEnforcerRules(target, '  ', 8, messageLink))
+    String[] immutableFiles = new File(target, 'immutable.files')
+    dispatcherPom.text = dispatcherPom.text.replaceAll('IMMUTABLE_RULES_NON_WINDOWS', getImmutableDispatcherFileEnforcerRules(target, immutableFiles, '  ', 10, messageLink, false))
+    dispatcherPom.text = dispatcherPom.text.replaceAll('IMMUTABLE_RULES_WINDOWS', getImmutableDispatcherFileEnforcerRules(target, immutableFiles, '  ', 10, messageLink, true))
+    assert new File(target, 'immutable.files').delete()
 }
 
-def md5(file) {
+def md5(file, isWindowsLineEnding) {
     def hash = MessageDigest.getInstance('MD5')
-    file.eachByte( 8192 ) { buffer, num ->
-        hash.update(buffer, 0, num)
+    file.withReader { reader ->
+        while ((line = reader.readLine()) != null) {
+            // assume that even the last line ends with a newline character
+            line += isWindowsLineEnding ? "\r\n" : "\n"
+            def buffer = line.getBytes(StandardCharsets.UTF_8)
+            hash.update(buffer, 0, buffer.length)
+        }
     }
     hash.digest().encodeHex().toString()
 }
 
-def getImmutableDispatcherFileEnforcerRules(baseDir, indent, initialIndentLevel, messageLink) {
-    // read file names from 
-    String[] immutableFiles = new File(baseDir, 'immutable.files')
-    assert new File(baseDir, 'immutable.files').delete()
+def getImmutableDispatcherFileEnforcerRules(baseDir, immutableFiles, indent, initialIndentLevel, messageLink, isWindowsLineEnding) {
     StringWriter rules = new StringWriter()
     IndentPrinter printer = new IndentPrinter(rules, indent, true, true)
     printer.setIndentLevel(initialIndentLevel)
@@ -34,7 +37,7 @@ def getImmutableDispatcherFileEnforcerRules(baseDir, indent, initialIndentLevel,
         printer.println('<requireFileChecksum>')
         printer.incrementIndent();
         printer.println("<file>${immutableFile}</file>")
-        def md5 = md5(new File(baseDir, immutableFile))
+        def md5 = md5(new File(baseDir, immutableFile), isWindowsLineEnding)
         printer.println("<checksum>${md5}</checksum>")
         printer.println('<type>md5</type>')
         printer.println("<message>There have been changes detected in a file which is supposed to be immutable according to ${messageLink}: ${immutableFile}</message>")
