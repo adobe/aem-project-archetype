5e180f6d6ee366358fe8d0b2076f800ade462427
Niek Raaijmakers <raaijmak@adobe.com>
2021-01-27 13:06:30 +0100

    Add core components to react and angular

--- src/main/archetype/ui.frontend.angular/src/app/app.module.ts
+++ src/main/archetype/ui.frontend.angular/src/app/app.module.ts
@@ -25,15 +25,46 @@ import { ModelManagerService } from './components/model-manager.service';
 import { PageComponent } from './components/page/page.component';
 import { TextComponent } from './components/text/text.component';
 
+import {AemAngularCoreWcmComponentsTabsV1} from '@adobe/aem-core-components-angular-spa/containers/tabs/v1';
+
+import {AemAngularCoreWcmComponentsTitleV2} from '@adobe/aem-core-components-angular-base/authoring/title/v2';
+import {AemAngularCoreWcmComponentsBreadCrumbV2} from '@adobe/aem-core-components-angular-base/layout/breadcrumb/v2';
+import {AemAngularCoreWcmComponentsNavigationV1} from '@adobe/aem-core-components-angular-base/layout/navigation/v1';
+import {AemAngularCoreWcmComponentsButtonV1} from '@adobe/aem-core-components-angular-base/authoring/button/v1';
+import {AemAngularCoreWcmComponentsImageV2} from '@adobe/aem-core-components-angular-base/authoring/image/v2';
+
+import {AemAngularCoreWcmComponentsTeaserV1} from '@adobe/aem-core-components-angular-base/authoring/teaser/v1';
+import {AemAngularCoreWcmComponentsDownloadV1} from '@adobe/aem-core-components-angular-base/authoring/download/v1';
+
+import {AemAngularCoreWcmComponentsListV2} from '@adobe/aem-core-components-angular-base/authoring/list/v2';
+import {AemAngularCoreWcmComponentsSeparatorV1} from '@adobe/aem-core-components-angular-base/authoring/separator/v1';
+import {AemAngularCoreWcmComponentsAccordionV1} from '@adobe/aem-core-components-angular-spa/containers/accordion/v1';
+import {AemAngularCoreWcmComponentsCarouselV1} from '@adobe/aem-core-components-angular-spa/containers/carousel/v1';
+import {AemAngularCoreWcmComponentsLanguageNavigationV1} from '@adobe/aem-core-components-angular-base/layout/language-navigation/v1';
+
 @NgModule({
   imports: [
     BrowserModule,
     SpaAngularEditableComponentsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AemAngularCoreWcmComponentsTabsV1,
+    AemAngularCoreWcmComponentsTitleV2,
+    AemAngularCoreWcmComponentsBreadCrumbV2,
+    AemAngularCoreWcmComponentsNavigationV1,
+    AemAngularCoreWcmComponentsButtonV1,
+    AemAngularCoreWcmComponentsImageV2,
+    AemAngularCoreWcmComponentsTeaserV1,
+    AemAngularCoreWcmComponentsDownloadV1,
+    AemAngularCoreWcmComponentsListV2,
+    AemAngularCoreWcmComponentsSeparatorV1,
+    AemAngularCoreWcmComponentsAccordionV1,
+    AemAngularCoreWcmComponentsCarouselV1,
+    AemAngularCoreWcmComponentsLanguageNavigationV1
   ],
-  providers: [ModelManagerService, { provide: APP_BASE_HREF, useValue: '/' }],
+  providers: [ ModelManagerService,
+    { provide: APP_BASE_HREF, useValue: '/' } ],
   declarations: [AppComponent, TextComponent, PageComponent],
   entryComponents: [TextComponent, PageComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule {}
--- src/main/archetype/ui.frontend.angular/src/app/components/import-components.ts
+++ src/main/archetype/ui.frontend.angular/src/app/components/import-components.ts
@@ -13,6 +13,42 @@
  ~ See the License for the specific language governing permissions and
  ~ limitations under the License.
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+import {MapTo} from '@adobe/aem-angular-editable-components';
 
 import './container/container.component';
 import './responsive-grid/responsive-grid.component';
+
+import {TabsV1Component} from '@adobe/aem-core-components-angular-spa/containers/tabs/v1';
+
+import {TitleV2Component, TitleV2IsEmptyFn} from '@adobe/aem-core-components-angular-base/authoring/title/v2';
+import {BreadCrumbV2Component, BreadCrumbV2IsEmptyFn} from '@adobe/aem-core-components-angular-base/layout/breadcrumb/v2';
+import {NavigationV1Component, NavigationV1IsEmptyFn} from '@adobe/aem-core-components-angular-base/layout/navigation/v1';
+import {ButtonV1Component, ButtonV1IsEmptyFn} from '@adobe/aem-core-components-angular-base/authoring/button/v1';
+import {ImageV2Component, ImageV2IsEmptyFn} from '@adobe/aem-core-components-angular-base/authoring/image/v2';
+
+import {TeaserV1Component, TeaserV1IsEmptyFn} from '@adobe/aem-core-components-angular-base/authoring/teaser/v1';
+import {DownloadV1Component, DownloadV1IsEmptyFn} from '@adobe/aem-core-components-angular-base/authoring/download/v1';
+
+import {ListV2Component, ListV2IsEmptyFn} from '@adobe/aem-core-components-angular-base/authoring/list/v2';
+import {SeparatorV1Component} from '@adobe/aem-core-components-angular-base/authoring/separator/v1';
+import {AccordionV1Component} from '@adobe/aem-core-components-angular-spa/containers/accordion/v1';
+import {CarouselV1Component} from '@adobe/aem-core-components-angular-spa/containers/carousel/v1';
+import {LanguageNavigationV1Component} from '@adobe/aem-core-components-angular-base/layout/language-navigation/v1';
+
+
+MapTo('${projectName}/components/navigation')(NavigationV1Component, {isEmpty: NavigationV1IsEmptyFn});
+MapTo('${projectName}/components/teaser')(TeaserV1Component, {isEmpty: TeaserV1IsEmptyFn});
+MapTo('${projectName}/components/title')(TitleV2Component, {isEmpty: TitleV2IsEmptyFn});
+MapTo('${projectName}/components/separator')(SeparatorV1Component);
+
+
+MapTo('${projectName}/components/download')(DownloadV1Component,{isEmpty: DownloadV1IsEmptyFn});
+MapTo('${projectName}/components/languagenavigation')(LanguageNavigationV1Component);
+MapTo('${projectName}/components/list')(ListV2Component, {isEmpty: ListV2IsEmptyFn});
+MapTo('${projectName}/components/breadcrumb')(BreadCrumbV2Component, {isEmpty: BreadCrumbV2IsEmptyFn})
+MapTo('${projectName}/components/button')(ButtonV1Component, {isEmpty: ButtonV1IsEmptyFn});
+MapTo('${projectName}/components/image')(ImageV2Component, {isEmpty: ImageV2IsEmptyFn});
+
+MapTo('${projectName}/components/carousel')(CarouselV1Component);
+MapTo('${projectName}/components/accordion')(AccordionV1Component);
+MapTo('${projectName}/components/tabs')(TabsV1Component);
