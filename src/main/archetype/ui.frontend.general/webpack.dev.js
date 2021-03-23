f61f85d3fc02623f074dceb4a19cff6d8afb54b4
Robin Alaerts <robin.alaerts@aginsurance.be>
2021-03-23 19:18:09 +0100

    - add support for injection of webpack assets into AEM page with a normal webpack devserver - only include selected clientlibs in the proxy config - remove writeToDisk option - add injectScripts middleware - add default publicPath, required for injectScripts

--- src/main/archetype/ui.frontend.general/webpack.dev.js
+++ src/main/archetype/ui.frontend.general/webpack.dev.js
@@ -2,16 +2,17 @@ const merge             = require('webpack-merge');
 const common            = require('./webpack.common.js');
 const path              = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const injectScripts     = require('webpack-dev-server-inject-scripts');
 
 const SOURCE_ROOT = __dirname + '/src/main/webpack';
 
 module.exports = env => {
-    const writeToDisk = env && Boolean(env.writeToDisk);
 
     return merge(common, {
         mode: 'development',
         devtool: 'inline-source-map',
         performance: { hints: 'warning' },
+        publicPath: '/',
         plugins: [
             new HtmlWebpackPlugin({
                 template: path.resolve(__dirname, SOURCE_ROOT + '/static/index.html')
@@ -20,11 +21,15 @@ module.exports = env => {
         devServer: {
             inline: true,
             proxy: [{
-                context: ['/content', '/etc.clientlibs'],
+                context: [
+                    '/content',
+                    '/etc.clientlibs/opp/clientlibs/clientlib-base.*.*'
+                ],
                 target: 'http://localhost:4502',
             }],
-            writeToDisk,
-            liveReload: !writeToDisk
+            before: (app, server, compiler) => {
+                app.use(injectScripts(compiler));
+            };
         }
     });
 }
