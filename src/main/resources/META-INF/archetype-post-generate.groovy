b23cf1ccc8ffb40f9869879778778fbc1ea90503
sakshi-arora1 <80878797+sakshi-arora1@users.noreply.github.com>
2021-03-24 19:33:23 +0530

    CQ-4303844 Add sample adaptive forms theme and templates via archetype. (#659)
    * CQ-4303844 Add sample adaptive forms theme and templates via archetype.
    
    * CQ-4303844 Add sample adaptive forms theme and templates via archetype. Fixed policy check.
    
    * CQ-4303844 Add sample adaptive forms theme and templates via archetype. Added sample image filters exclusively.
    
    * CQ-4303844 Add sample adaptive forms theme and templates via archetype. Added jcr last modified to theme and templates, to enable forms caching.
    
    Co-authored-by: Sakshi Arora <sakarora@Sakshis-MacBook-Pro.local>

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -169,9 +169,15 @@ if (includeCommerce == "n") {
     }
 }
 
-// if forms flag is not set, forms specific content/configuration/proxy components should be deleted
+// if forms flag is not set, forms specific components, template-types, templates, themes should be deleted
 if (includeForms == "n") {
     assert new File("$appsFolder/components/aemformscontainer").deleteDir()
+    assert new File("$confFolder/settings/wcm/template-types/af-page").deleteDir()
+    assert new File("$confFolder/settings/wcm/templates/basic-af").deleteDir()
+    assert new File("$confFolder/settings/wcm/templates/blank-af").deleteDir()
+    assert new File("$uiContentPackage/src/main/content/jcr_root/content/dam/formsanddocuments-themes").deleteDir()
+    assert new File("$uiContentPackage/src/main/content/jcr_root/content/dam/$appId/sample_logo.png").deleteDir()
+    assert new File("$uiContentPackage/src/main/content/jcr_root/content/dam/$appId/sample_terms.png").deleteDir()
 } else {
     if (aemVersion == "cloud") {
         // if forms is included and aem version is set to cloud, set the forms sdk version
