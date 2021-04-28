d6a827ae2d7ef774e5831e9d66ee4109d6a97b55
Niek Raaijmakers <raaijmak@adobe.com>
2021-04-28 06:46:07 +0200

    remote warning

--- src/main/archetype/ui.frontend.react/src/server/CustomModelClient.js
+++ src/main/archetype/ui.frontend.react/src/server/CustomModelClient.js
@@ -13,7 +13,6 @@
  ~ See the License for the specific language governing permissions and
  ~ limitations under the License.
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-import fetch from "node-fetch";
 import { ModelClient } from '@adobe/aem-spa-page-model-manager';
 
 
