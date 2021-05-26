fd738ebd474cc51799e490664ff418a063f0390c
pfauchere <patrick.fauchere@gmail.com>
2021-05-27 00:26:53 +0900

    Revert "IO runtime module for react (#689)"
    This reverts commit f5ecdab99e4a3638c6efa8947a8454dadec7f78a.

--- src/main/archetype/ui.frontend.react/src/index.js
+++ src/main/archetype/ui.frontend.react/src/index.js
@@ -1,3 +1,22 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2020 Adobe Systems Incorporated
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+// Use polyfills for modern language features
+// The imports and dependencies can be removed if only modern browsers should be
+// supported
 import 'react-app-polyfill/stable';
 import 'react-app-polyfill/ie9';
 import 'custom-event-polyfill';
@@ -5,79 +24,27 @@ import 'custom-event-polyfill';
 import { Constants, ModelManager } from '@adobe/aem-spa-page-model-manager';
 import { createBrowserHistory } from 'history';
 import React from 'react';
-#if ( $enableSSR == "y")
-import { render, hydrate } from 'react-dom';
-#end
-#if ( $enableSSR == "n")
 import { render } from 'react-dom';
-#end
 import { Router } from 'react-router-dom';
 import App from './App';
 import './components/import-components';
 import './index.css';
 
-const renderApp = () => {
-    ModelManager.initialize().then(pageModel => {
-        const history = createBrowserHistory();
-        render(
-            <Router history={history}>
-                <App
-                    history={history}
-                    cqChildren={pageModel[Constants.CHILDREN_PROP]}
-                    cqItems={pageModel[Constants.ITEMS_PROP]}
-                    cqItemsOrder={pageModel[Constants.ITEMS_ORDER_PROP]}
-                    cqPath={pageModel[Constants.PATH_PROP]}
-                    locationPathname={window.location.pathname}
-                />
-            </Router>,
-            document.getElementById('spa-root')
-        );
-    });
-};
-
-#if ( $enableSSR == "y")
-const hydrateApp = (initialState) => {
-    ModelManager.initialize({
-        model: initialState.rootModel
-    }).then(pageModel => {
-        const history = createBrowserHistory();
-        hydrate(
-            <Router history={history}>
-                <App
-                    history={history}
-                    cqChildren={pageModel[Constants.CHILDREN_PROP]}
-                    cqItems={pageModel[Constants.ITEMS_PROP]}
-                    cqItemsOrder={pageModel[Constants.ITEMS_ORDER_PROP]}
-                    cqPath={pageModel[Constants.PATH_PROP]}
-                    locationPathname={window.location.pathname}
-                />
-            </Router>,
-            document.getElementById('spa-root')
-        );
-    });
-}
-#end
-
 document.addEventListener('DOMContentLoaded', () => {
-
-#if ( $enableSSR == "y")
-    const initialStateScriptTag = document.getElementById('__INITIAL_STATE__');
-    if(!!initialStateScriptTag){
-        try{
-            const initialState = JSON.parse(initialStateScriptTag.innerHTML);
-            initialStateScriptTag.remove();
-            hydrateApp(initialState);
-
-            console.info('hydrated react DOM');
-        }catch(err){
-            console.error('failed to parse initial state json! re-rendering output.', err);
-            renderApp();
-        }
-    }else{
-        renderApp();
-    }
-#end
-#if ( $enableSSR == "n")
-    renderApp();
-#end
+  ModelManager.initialize().then(pageModel => {
+    const history = createBrowserHistory();
+    render(
+      <Router history={history}>
+        <App
+          history={history}
+          cqChildren={pageModel[Constants.CHILDREN_PROP]}
+          cqItems={pageModel[Constants.ITEMS_PROP]}
+          cqItemsOrder={pageModel[Constants.ITEMS_ORDER_PROP]}
+          cqPath={pageModel[Constants.PATH_PROP]}
+          locationPathname={window.location.pathname}
+        />
+      </Router>,
+      document.getElementById('spa-root')
+    );
+  });
 });
