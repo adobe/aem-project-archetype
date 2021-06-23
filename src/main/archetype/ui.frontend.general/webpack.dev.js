715272fa62f54fade7947c39132473d8911b3505
Usman Khalid <usman.khalid@live.ca>
2021-06-23 09:21:42 -0400

    chore: optimize ui.frontend.general watch script (#688)
    Co-authored-by: Usman Khalid <usman.khalid@t4g.com>

--- src/main/archetype/ui.frontend.general/webpack.dev.js
+++ src/main/archetype/ui.frontend.general/webpack.dev.js
@@ -1,22 +1,30 @@
-const merge             = require('webpack-merge');
-const common            = require('./webpack.common.js');
-const path              = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge              = require('webpack-merge');
+const common             = require('./webpack.common.js');
+const path               = require('path');
+const HtmlWebpackPlugin  = require('html-webpack-plugin');
+const WebpackShellPlugin = require('webpack-shell-plugin');
 
 const SOURCE_ROOT = __dirname + '/src/main/webpack';
 
 module.exports = env => {
     const writeToDisk = env && Boolean(env.writeToDisk);
+    const plugins = [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, SOURCE_ROOT + '/static/index.html')
+      })
+    ];
+
+    writeToDisk && plugins.push(
+      new WebpackShellPlugin({
+        onBuildExit:['clientlib && npm run sync']
+      })
+    );
 
     return merge(common, {
         mode: 'development',
         devtool: 'inline-source-map',
         performance: { hints: 'warning' },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: path.resolve(__dirname, SOURCE_ROOT + '/static/index.html')
-            })
-        ],
+        plugins,
         devServer: {
             inline: true,
             proxy: [{
