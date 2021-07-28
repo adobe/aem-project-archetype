dc994e45a70eb58d02f014ffec4e3017555da061
Pankaj Parashar <pankajpr@adobe.com>
2021-07-28 13:22:14 +0530

    CQ-4321853 - e2e Test for templates / themes in aem archetype project
    Review comments fixes
    E2E tests added, basic sanity tests merged to E2E tests

--- src/main/archetype/ui.tests/test-module/lib/util/forms.selectors.js
+++ src/main/archetype/ui.tests/test-module/lib/util/forms.selectors.js
@@ -5,29 +5,12 @@ var selectors = {
             initial : '.editor-GlobalBar-layerSwitcherPopoverContent button[data-layer="initial"]',
             structure : '.editor-GlobalBar-layerSwitcherPopoverContent button[data-layer="structure"]'
         },
-        previewButtonSelector : '.editor-GlobalBar-previewTrigger',
-        previewLayerSelector : '.aem-AuthorLayer-Preview',
-        currentLayerButtonSelector : '.editor-GlobalBar-layerCurrent',
-        editorLayerSelector : 'html.aem-AuthorLayer-Theme.Edit',
-        editToolBar : {
-            configure : '#EditableToolbar button[data-action="CONFIGURE"]'
-        },
-        sidePanelToggleButton : '.toggle-sidepanel.editor-GlobalBar-item',
-        sidePanelOpen : '#SidePanel.sidepanel-opened',
-        sidePanel : {
-            afPropertiesContainer : '#sidepanel-guide-properties',
-            stylePropertySheetSelector : '#style-propertySheetContainer',
-            propertiesTab : 'coral-tab[title="Properties"]'
-        },
-        overlay : {
-            guideFieldLabelOvelaySelector : '#af-overlay-selector-text > button[data-target-selector="af_fieldlabel"]'
-        },
         themeEditor : {
             themeEditorObjSelector : '.themeEditableObject'
         },
         templateEditor : {
             structureLayer : {
-                policyButton : 'button.cq-editable-action[title="Policy"]',
+                policyButton : '.cq-editable-action[title="Policy"]',
                 policyPage : {
                     allowedComponent : {
                         adaptiveFormGroup : '.js-cq-AllowedComponents-groupCheckbox[value="group:Adaptive Form"]'
@@ -38,8 +21,7 @@ var selectors = {
         }
     },
     content : {
-        genericAFField : '.guideFieldNode',
-        guideFieldLabelSelector : '.guideFieldLabel > label',
+        genericAFField : '.guidefield'
     }
 
 };
--- src/main/archetype/ui.tests/test-module/specs/aem/forms.js
+++ src/main/archetype/ui.tests/test-module/specs/aem/forms.js
@@ -17,6 +17,8 @@
 const config = require('../../lib/config');
 const commons = require('../../lib/commons');
 const selectors = require('../../lib/util/forms.selectors.js');
+const fs = require('fs');
+const path = require('path');
 
 describe('AEM Forms Reference Artifacts', () => {
 
@@ -49,46 +51,99 @@ describe('AEM Forms Reference Artifacts', () => {
         onboardingHdler.disable();
     });
 
-
-    describe('AEM Forms Theme', () => {
-
-        it('Basic Testing of Canvas Theme', function () {
+    describe('AEM Forms Theme E2E', () => {
+        it('E2E Testing of Canvas 3.0 Theme', function () {
             testName = this.test.parent.title + '.' + this.test.title;
-            let themePath = '/content/dam/formsanddocuments-themes/${appId}/canvas-3-0/jcr:content';
+            let themePath = '/content/dam/formsanddocuments-themes/${appId}/canvas-3-0/jcr:content',
+                failedRules = [],
+                verifyCSS = function (selectors, cssProperty, expectedCSSValue) {
+                    let selectorList = selectors.split(',');
+                    selectorList.forEach(selector => {
+                        let actualCSSValue = $(selector).getCSSProperty(cssProperty).value;
+                        if ( actualCSSValue !== expectedCSSValue) {
+                            failedRules.push('Verification failed for selector ' + selector + ' and style rule ' + cssProperty + ' [Expected : ' + expectedCSSValue + ' but ' + 'Received : ' + actualCSSValue + ']');
+                        }
+                    });
+                };
 
             //Open Canvas theme in editing mode
             browser.url(`${config.aem.author.base_url}/editor.html${themePath}`);
 
-            //Verify Selectors list is visible
-            expect($(selectors.editor.themeEditor.themeEditorObjSelector).waitForDisplayed()).toBe(true);
-
             //Verify no console error present in edit mode so far
             expect(consoleErrors).toHaveLength(0);
 
-            //Click preview layer button
-            expect($(selectors.editor.previewButtonSelector)).toBeDisplayed();
-            browser.$(selectors.editor.previewButtonSelector).click();
+            //Preview theme
+            browser.url(`${config.aem.author.base_url}/${themePath}?wcmmode=disabled`);
 
-            //Switch to content frame and verify preview layer mode
-            browser.switchToFrame(0);
-            expect($(selectors.editor.previewLayerSelector).waitForDisplayed()).toBe(true);
+            let ruleJson = fs.readFileSync(path.resolve(__dirname, '../../rules/canvas-3.0-theme-rules.json'));
+            let rules = JSON.parse(ruleJson);
 
-            //Verify no console error present in preview mode
+            for (let selectors in rules.cssRules) {
+                let selectorRules = rules.cssRules[selectors];
+                for (let cssProperty in selectorRules ) {
+                    verifyCSS(selectors, cssProperty, selectorRules[cssProperty] );
+                }
+            }
+            //Print all error messages for all the failed rules
+            for (let failedRule of failedRules) {
+                console.error(failedRule);
+            }
+            expect(failedRules).toHaveLength(0);
+            //Verify console errors for preview mode
             expect(consoleErrors).toHaveLength(0);
 
         });
 
     });
 
-    describe('AEM Forms Templates', () => {
+    describe('AEM Forms Templates E2E Testing', () => {
         let templates = [
-                {path : '/conf/${appId}/settings/wcm/templates/basic-af', isBlankForm : false},
+                {path : '/conf/${appId}/settings/wcm/templates/basic-af', isBlankForm : false, verificationRuleFilePath : '../../rules/template-rules.json'},
                 {path : '/conf/${appId}/settings/wcm/templates/blank-af', isBlankForm : true}
             ],
+            tabbedLayoutCount = 0,
+            tabNavigatorSelector = '.tab-navigators-vertical',
             getContainerSelector = (templateContentPath, container) => {
                 return `div[data-path="${templateContentPath}/${container}"]`;
             },
-            verifyTemplate = (templatePath, isBlankForm) => {
+            getVisibleFieldCount = function () {
+                var fields = browser.$$('.tab-pane.active ' + selectors.content.genericAFField);
+                let count = 0;
+                for (let field of fields){
+                    if(field.isDisplayed()){
+                        count++;
+                    }
+                }
+                return count;
+            },
+            verifyTabbedPanelContent = function (panel) {
+                if (panel.layout === 'tabsLeft' ) {
+                    tabbedLayoutCount++;
+                    let tabbedLayoutLevel = tabbedLayoutCount,
+                        items = panel.items;
+                    if (items instanceof Array ) {
+                        //Verify No. of tabs
+                        expect(browser.$$(tabNavigatorSelector + ':nth-child(' + tabbedLayoutCount + ')>li')).toHaveLength(items.length);
+                        for (let i = 0; i < items.length; i++ ) {
+                            let childPanel = items[i];
+                            if (childPanel.layout === 'Responsive' && !(childPanel.items instanceof Array)) {
+                                //Navigate through all the panels by clicking navigator tab
+                                browser.$$(tabNavigatorSelector + ':nth-child(' + tabbedLayoutLevel + ')>li')[[i]].waitForClickable();
+                                browser.$$(tabNavigatorSelector + ':nth-child(' + tabbedLayoutLevel + ')>li')[[i]].click();
+                                //Verify field count for each panel
+                                expect(getVisibleFieldCount()).toBe(childPanel.items.count);
+                            } else {
+                                verifyTabbedPanelContent(items[i]);
+                            }
+                        }
+                    }
+                }
+
+                //Verify total number of tabbed Layout in the form
+                expect(browser.$$(tabNavigatorSelector)).toHaveLength(tabbedLayoutCount);
+
+            },
+            verifyTemplate = (templatePath, isBlankForm, verificationRuleFilePath) => {
                 let templateStructure = templatePath + '/structure/jcr:content',
                     templateInitialContent = templatePath + '/initial/jcr:content',
                     afContainerOverlaySelector = '#OverlayWrapper ' + getContainerSelector(templateInitialContent,'guideContainer');
@@ -97,54 +152,56 @@ describe('AEM Forms Reference Artifacts', () => {
                 browser.url(`${config.aem.author.base_url}/editor.html${templatePath}/structure.html`);
 
                 //First open structure layer
-                expect($(selectors.editor.layerSwitcher).waitForDisplayed()).toBe(true);
+                $(selectors.editor.layerSwitcher).waitForClickable();
                 $(selectors.editor.layerSwitcher).click();
-                expect($(selectors.editor.layerSelector.structure).waitForDisplayed()).toBe(true);
+                $(selectors.editor.layerSelector.structure).waitForClickable();
                 $(selectors.editor.layerSelector.structure).click();
 
-                //Verify all three containers are visible
+                //Content Verification - Verify all three containers are visible in structure
                 expect($(getContainerSelector(templateStructure,'parsys1'))).toBeDisplayed();
                 expect($(getContainerSelector(templateStructure,'parsys2'))).toBeDisplayed();
                 expect($(getContainerSelector(templateStructure,'guideContainer'))).toBeDisplayed();
 
-                //Verify Applied Policy Content
+                //Content Verification - Verify Applied Policy
+                $(getContainerSelector(templateStructure,'guideContainer')).waitForClickable();
                 $(getContainerSelector(templateStructure,'guideContainer')).click();
-                expect($(selectors.editor.templateEditor.structureLayer.policyButton).waitForDisplayed()).toBe(true);
+                $(selectors.editor.templateEditor.structureLayer.policyButton).waitForClickable();
                 $(selectors.editor.templateEditor.structureLayer.policyButton).click();
                 expect($(selectors.editor.templateEditor.structureLayer.policyPage.allowedComponent.adaptiveFormGroup + '[checked]').waitForDisplayed()).toBe(true);
-                expect($(selectors.editor.templateEditor.structureLayer.policyPage.cancel).waitForDisplayed()).toBe(true);
+                $(selectors.editor.templateEditor.structureLayer.policyPage.cancel).waitForClickable();
                 $(selectors.editor.templateEditor.structureLayer.policyPage.cancel).click();
 
-                //Switch to initial content layer
-                expect($(selectors.editor.layerSwitcher).waitForDisplayed()).toBe(true);
+                //Switch to initial content layer to just check any console error in edit layer
+                $(selectors.editor.layerSwitcher).waitForClickable();
                 $(selectors.editor.layerSwitcher).click();
-                expect($(selectors.editor.layerSelector.initial).waitForDisplayed()).toBe(true);
+                $(selectors.editor.layerSelector.initial).waitForClickable();
                 $(selectors.editor.layerSelector.initial).click();
                 expect($(afContainerOverlaySelector).waitForDisplayed()).toBe(true);
 
                 //Verify no console error present in structure and initial content layer
                 expect(consoleErrors).toHaveLength(0);
 
-                //Open Side panel, verify that no invalid item in properties tab and then close the side panel
-                expect($(selectors.editor.sidePanelToggleButton).waitForDisplayed()).toBe(true);
-                $(selectors.editor.sidePanelToggleButton).click();
-                expect($(selectors.editor.sidePanelOpen).waitForDisplayed()).toBe(true);
-                expect($(selectors.editor.sidePanel.propertiesTab + '.is-invalid').waitForDisplayed({reverse : true})).toBe(true);
-                $(selectors.editor.sidePanelToggleButton).click();
-                expect($(selectors.editor.sidePanelOpen).waitForDisplayed({reverse : true})).toBe(true);
-
-                //Preview the template using url and verify AF field presence in the form
+                //Preview the template using url and verify no. of AF field presence in the form/panel
                 browser.url(`${config.aem.author.base_url}/${templatePath}/initial.html?wcmmode=preview`);
-                expect($(selectors.content.genericAFField).waitForDisplayed({ reverse : isBlankForm })).toBe(true);
+                if(isBlankForm) {
+                    //For blank template, there should be no AF field present
+                    expect($(selectors.content.genericAFField).waitForDisplayed({ reverse : isBlankForm })).toBe(true);
+                }else {
+                    //Load the verification rule and validate content
+                    let ruleJson = fs.readFileSync(path.resolve(__dirname, verificationRuleFilePath));
+                    let rules = JSON.parse(ruleJson);
+                    tabbedLayoutCount = 0;
+                    verifyTabbedPanelContent(rules.template.content.formContainer.rootPanel);
+                }
 
                 //Verify no console error present in preview mode
                 expect(consoleErrors).toHaveLength(0);
             };
 
         templates.forEach(function (template) {
-            it('Basic Testing of Template '+ template.path , function () {
+            it('E2E Testing of Template '+ template.path , function () {
                 testName = this.test.parent.title + '.' + this.test.title;
-                verifyTemplate(template.path, template.isBlankForm);
+                verifyTemplate(template.path, template.isBlankForm, template.verificationRuleFilePath);
             });
         });
 
