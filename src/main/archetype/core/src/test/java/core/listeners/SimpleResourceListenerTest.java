17a6997d5068baf559b049f9c8a89eee384d89ef
Joerg Hoh <jhoh@adobe.com>
2021-12-05 21:43:08 +0100

    #841 move to the recommended ResourceChangeListener

--- src/main/archetype/core/src/test/java/core/listeners/SimpleResourceListenerTest.java
+++ src/main/archetype/core/src/test/java/core/listeners/SimpleResourceListenerTest.java
@@ -15,20 +15,20 @@
  */
 package ${package}.core.listeners;
 
-import java.util.Collections;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
 import java.util.List;
 
-import org.apache.sling.api.SlingConstants;
+import org.apache.sling.api.resource.observation.ResourceChange;
+import org.apache.sling.api.resource.observation.ResourceChange.ChangeType;
 import org.junit.jupiter.api.Test;
-import org.osgi.service.event.Event;
 
 import uk.org.lidalia.slf4jext.Level;
 import uk.org.lidalia.slf4jtest.LoggingEvent;
 import uk.org.lidalia.slf4jtest.TestLogger;
 import uk.org.lidalia.slf4jtest.TestLoggerFactory;
-
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SimpleResourceListenerTest {
 
@@ -38,9 +38,10 @@ class SimpleResourceListenerTest {
 
     @Test
     void handleEvent() {
-        Event resourceEvent = new Event("event/topic", Collections.singletonMap(SlingConstants.PROPERTY_PATH, "/content/test"));
-
-        fixture.handleEvent(resourceEvent);
+        
+        ResourceChange change = new ResourceChange(ChangeType.ADDED,"/content/test", false);
+        
+        fixture.onChange(Arrays.asList(change));
 
         List<LoggingEvent> events = logger.getLoggingEvents();
         assertEquals(1, events.size());
@@ -48,9 +49,10 @@ class SimpleResourceListenerTest {
 
         assertAll(
                 () -> assertEquals(Level.DEBUG, event.getLevel()),
-                () -> assertEquals(2, event.getArguments().size()),
-                () -> assertEquals("event/topic", event.getArguments().get(0)),
-                () -> assertEquals("/content/test", event.getArguments().get(1))
+                () -> assertEquals(3, event.getArguments().size()),
+                () -> assertEquals(ChangeType.ADDED, event.getArguments().get(0)),
+                () -> assertEquals("/content/test", event.getArguments().get(1)),
+                () -> assertEquals(Boolean.FALSE,event.getArguments().get(2))
         );
     }
 }
