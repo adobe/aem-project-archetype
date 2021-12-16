937831ae96ecdec19bbcc9186a4cb1e00bffc32e
Stefan Seifert <stefanseifert@users.noreply.github.com>
2021-12-16 12:27:09 +0100

    #847 Update to latest AEM Mocks version (which includes some important updates from sling-mocks osgi-mock) (#848)
    include Sling Context-Aware Configuration plugin to test code which uses context-aware configuration
    refactor AppAemContext a bit to make it easier to test with different resource resolver types
    also update JUnit and Mockito testing dependencies

--- src/main/archetype/core/src/test/java/core/testcontext/AppAemContext.java
+++ src/main/archetype/core/src/test/java/core/testcontext/AppAemContext.java
@@ -16,6 +16,9 @@
 package ${package}.core.testcontext;
 
 import static com.adobe.cq.wcm.core.components.testing.mock.ContextPlugins.CORE_COMPONENTS;
+import static org.apache.sling.testing.mock.caconfig.ContextPlugins.CACONFIG;
+
+import org.apache.sling.testing.mock.sling.ResourceResolverType;
 
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextBuilder;
@@ -34,10 +37,24 @@ public final class AppAemContext {
      * @return {@link AemContext}
      */
     public static AemContext newAemContext() {
+        return newAemContextBuilder().build();
+    }
+
+    /**
+     * @return {@link AemContextBuilder}
+     */
+    public static AemContextBuilder newAemContextBuilder() {
+        return newAemContextBuilder(ResourceResolverType.RESOURCERESOLVER_MOCK);
+    }
+
+    /**
+     * @return {@link AemContextBuilder}
+     */
+    public static AemContextBuilder newAemContextBuilder(ResourceResolverType resourceResolverType) {
         return new AemContextBuilder()
+                .plugin(CACONFIG)
                 .plugin(CORE_COMPONENTS)
-                .afterSetUp(SETUP_CALLBACK)
-                .build();
+                .afterSetUp(SETUP_CALLBACK);
     }
 
     /**
