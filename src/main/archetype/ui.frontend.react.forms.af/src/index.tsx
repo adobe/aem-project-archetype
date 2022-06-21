2a4c967d153b62d3cf8910a137793c3578d5997c
Barshat Rai <barshatrai99@gmail.com>
2022-06-21 18:19:11 +0530

    CQ-4343453 Exclude forms core components if cloud (#949)
    * CQ-4343453 Exclude forms core components if cloud
    
    * CQ-4343453 include sdk api for cloud
    
    * CQ-4343453 minor changes
    
    Co-authored-by: barshatr <barshatr@adobe.com>

--- src/main/archetype/ui.frontend.react.forms.af/src/index.tsx
+++ src/main/archetype/ui.frontend.react.forms.af/src/index.tsx
@@ -3,25 +3,6 @@ import ReactDOM from 'react-dom';
 // @ts-ignore
 import AForm from './Form'
 
-const base64url = (s: any) => {
-    var to64url = btoa(s);
-    // Replace non-url compatible chars with base64url standard chars and remove leading =
-    return to64url.replace(/\+/g, '_').replace(/\//g, '-').replace(/=+${symbol_dollar}/g, '');
-}
-
-
-const getId = () => {
-    let id =  ""
-    if (!process.env.FORMPATH) {
-        const parent = document.querySelector(".cmp-formcontainer")
-        id = parent.getAttribute("data-form-id")
-    } else {
-        id = base64url(process.env.FORMPATH)
-    }
-    return id;
-}
-
-
 window.onload = async () => {
     let div = document.getElementById("form-app");
     if (!div) {
@@ -30,7 +11,7 @@ window.onload = async () => {
         document.body.appendChild(div)
     }
     ReactDOM.render(
-        <AForm id={getId()}/>,
+        <AForm/>,
         div
     );
 };
