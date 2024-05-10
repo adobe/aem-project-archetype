7f152a493a26d87fbb33fdedae60539da042a130
Lukasz Szczedzina <sii33416@adobe.com>
2024-05-10 14:22:00 +0200

    CQ-4355928 ui-tests-cypress: setup proxy environment variables

--- src/main/archetype/ui.tests.cypress/test-module/run.sh
+++ src/main/archetype/ui.tests.cypress/test-module/run.sh
@@ -23,5 +23,24 @@ echo 'checking Xvfb'
 ps aux | grep Xvfb
 # disable color output when running Cypress
 export NO_COLOR=1
+
+# setup proxy environment variables
+if [ -n "${PROXY_HOST:-}" ]; then
+  if [ -n "${PROXY_HTTPS_PORT:-}" ]; then
+    export HTTP_PROXY="https://${PROXY_HOST}:${PROXY_HTTPS_PORT}"
+  elif [ -n "${PROXY_HTTP_PORT:-}" ]; then
+    export HTTP_PROXY="http://${PROXY_HOST}:${PROXY_HTTP_PORT}"
+  fi
+  if [ -n "${PROXY_CA_PATH:-}" ]; then
+    export NODE_EXTRA_CA_CERTS=${PROXY_CA_PATH}
+  fi
+  if [ -n "${PROXY_OBSERVABILITY_PORT:-}" ] && [ -n "${HTTP_PROXY:-}" ]; then
+    echo "Waiting for proxy"
+    curl --silent  --retry ${PROXY_RETRY_ATTEMPTS:-3} --retry-connrefused --retry-delay ${PROXY_RETRY_DELAY:-10} \
+      --proxy ${HTTP_PROXY} --proxy-cacert ${PROXY_CA_PATH:-""} \
+      ${PROXY_HOST}:${PROXY_OBSERVABILITY_PORT}
+  fi
+fi
+
 # execute tests
 npm test
